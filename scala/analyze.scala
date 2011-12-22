c670544d26de18bdb7cefff79e19293ab78ee0f5
Avi Flax <avi@aviflax.com>
2011-12-22 10:43:46 -0500

    Scala: figured out how to make rollup more functional

--- scala/analyze.scala
+++ scala/analyze.scala
@@ -33,7 +33,6 @@ import java.text.SimpleDateFormat
 import java.util.Date
 import org.joda.time.Interval
 import org.joda.time.DateTime
-import java.util.concurrent.atomic.AtomicInteger
 
 
 def extractDate(line:String):DateTime = {
@@ -43,29 +42,25 @@ def extractDate(line:String):DateTime = {
     
     dateFormat.parse(dateString)
     
-    null
+    null // TODO: return a Joda DateTime
 }
 
 
-def makeWindow(date:DateTime, windowSpec:String, startCount:Int = 0) : (Interval, AtomicInteger) = null
+def makeWindow(date:DateTime, windowSpec:String, startCount:Int = 0) : (Interval, Int) = null
 
 
-def rollup(windows:List[(Interval, AtomicInteger)], date:DateTime, windowSpec:String = "1d") : List[(Interval, AtomicInteger)] = {
-    var result:List[(Interval, AtomicInteger)] = windows
-
-    windows.find(_._1.contains(date)) match {
-        case Some(window) => window._2.incrementAndGet()
-        case None => result = windows :+ makeWindow(date, windowSpec, 1)
-    }
-    
-    return result
+def rollup(windows:List[(Interval, Int)], date:DateTime, windowSpec:String = "1d") : List[(Interval, Int)] = {
+    if (windows.exists(_._1.contains(date)))
+        windows.map(window => if (window._1.contains(date)) (window._1, window._2 + 1) else window)
+    else
+        windows :+ makeWindow(date, windowSpec, 1)
 }
 
 
-def toCsv(windows:List[(Interval, AtomicInteger)]):String = { null }
+def toCsv(windows:List[(Interval, Int)]):String = null // TODO: return the CSV
 
 
-var windows:List[(Interval, AtomicInteger)] = List()
+var windows = List[(Interval, Int)]()
 
 println("Processing input")
 
